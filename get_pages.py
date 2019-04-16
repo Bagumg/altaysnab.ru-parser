8c6b41e7926b33087a059ee8320e2f4cac6f6e63
Yriy <41549026+Bagumg@users.noreply.github.com>
2019-04-16 22:53:03 +0700

    get_pages.py формирует отдельный файл с ссылками

--- get_pages.py
+++ get_pages.py
@@ -30,6 +30,73 @@ import csv
 # print(final_link)
 
 
+# Ниже идёт полностью рабочая версия
+
+# user_agent = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 '
+#                             'Safari/537.36'})
+#
+# def get_html(url):
+#     catalog_request = requests.get(url, user_agent)
+#     return catalog_request.text
+#
+#
+# # Получаем все ссылки на группы товаров в каталоге и добавляем их в массив
+# def get_all_links(html):
+#     soup = BeautifulSoup(html, 'lxml')
+#     tools = soup.find_all('div', class_='catalog-section-child')
+#     tools_links = []
+#     for td in tools:
+#         a = td.find('a').get('href')
+#         link = 'http://altaysnab.ru' + a
+#         tools_links.append(link)
+#     return tools_links
+#
+#
+# url = 'http://altaysnab.ru/catalog/'
+# all_links = get_all_links(get_html(url))
+# req_pack = []
+# link = []
+# final_link = []
+#
+#
+# # Погружаемся в пучину ада. Всё глубже и глубже
+# for i in all_links:
+#     time.sleep(0.3)
+#     r = requests.get(i)
+#     soup = BeautifulSoup(r.text, 'lxml')
+#     try:
+#         lis = soup.find('div', class_='pagination').find_all('a')
+#     except:
+#         lis = i
+#
+#     for li in lis:
+#         try:
+#             a = li.get('href')
+#             link.append('http://altaysnab.ru' + a)
+#         except:
+#             link.append(i)
+#
+#     last_page = re.findall(r'=(\d+)', link[-2])
+#
+#     try:
+#         for j in range(1, int(last_page[0]) + 1):
+#             final_link.append(f'{i}?PAGEN_1={j}')
+#     except:
+#         final_link.append(i)
+#
+#     link = []
+#
+#     print(final_link)
+#
+# with open('links.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     for link in final_link:
+#         writer.writerow([link])
+
+
+# Выше идёт поностью рабочая версия
+
+
 
 user_agent = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 '
                             'Safari/537.36'})
@@ -46,54 +113,61 @@ def get_all_links(html):
     tools_links = []
     for td in tools:
         a = td.find('a').get('href')
-        link = 'http://altaysnab.ru' + a
-        tools_links.append(link)
+        altsnab_link = 'http://altaysnab.ru' + a
+        tools_links.append(altsnab_link)
     return tools_links
 
 
-url = 'http://altaysnab.ru/catalog/'
-all_links = get_all_links(get_html(url))
-
-
-# print(all_links)
+# url = 'http://altaysnab.ru/catalog/'
+# all_links = get_all_links(get_html(url))
 req_pack = []
-link = []
+
 final_link = []
 
 
 # Погружаемся в пучину ада. Всё глубже и глубже
-for i in all_links:
-    # print(i)
-    time.sleep(0.3)
-    r = requests.get(i)
-    # print(r)
-    soup = BeautifulSoup(r.text, 'lxml')
-    try:
-        lis = soup.find('div', class_='pagination').find_all('a')
-    except:
-        lis = i
-
-    for li in lis:
-        try:
-            a = li.get('href')
-            link.append('http://altaysnab.ru' + a)
-        except:
-            link.append(i)
-
-    last_page = re.findall(r'=(\d+)', link[-2])
-
-    try:
-        for j in range(1, int(last_page[0]) + 1):
-            final_link.append(f'{i}?PAGEN_1={j}')
-    except:
-        final_link.append(i)
-
+def get_final_links(all_links):
     link = []
+    for i in all_links:
+        time.sleep(0.3)
+        r = requests.get(i)
+        soup = BeautifulSoup(r.text, 'lxml')
+        try:
+            lis = soup.find('div', class_='pagination').find_all('a')
+        except:
+            lis = i
 
-    print(final_link)
+        for li in lis:
+            try:
+                a = li.get('href')
+                link.append('http://altaysnab.ru' + a)
+            except:
+                link.append(i)
 
-with open('links.csv', 'w') as f:
-    writer = csv.writer(f)
-    for link in final_link:
-        writer.writerow([link])
-# print(final_link)
+        last_page = re.findall(r'=(\d+)', link[-2])
+
+        try:
+            for j in range(1, int(last_page[0]) + 1):
+                final_link.append(f'{i}?PAGEN_1={j}')
+        except:
+            final_link.append(i)
+
+        link = []
+    return final_link
+
+def write_final_links(final_link):
+    with open('links.csv', 'w') as f:
+        writer = csv.writer(f)
+        for link in final_link:
+            writer.writerow([link])
+
+
+def main():
+    url = 'http://altaysnab.ru/catalog/'
+    all_links = get_all_links(get_html(url))
+    final_link = get_final_links(all_links)
+    write_final_links(final_link)
+
+
+if __name__ == '__main__':
+    main()
